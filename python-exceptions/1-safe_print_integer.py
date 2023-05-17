994e19e02c741ed614b44a85d2b4d5fcd8daeefd
root <root@MSI>
2023-05-16 21:09:56 -0500

    adadsfg

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,10 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        i = int(value)
-        if isinstance(i, int):
-            print("{:d}".format(i))
-            return True
+        print("{:d}".format(value))
+        return (True)
     except (TypeError, ValueError):
-        return False
-    return False
+        return (False)
