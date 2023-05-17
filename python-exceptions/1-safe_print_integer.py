e9762c23ea35832e10a767383ce889fed837d5a7
root <root@MSI>
2023-05-16 21:03:17 -0500

    hmmm

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,8 @@
 
 def safe_print_integer(value):
     try:
-        print("{:d}".format(int(value)))
+        i = int(value)
+        print("{:d}".format(i))
         return True
     except (TypeError, ValueError):
         return False
