e6fe7c2cb0ce51fc5f0b1bab97a1828a959e9987
root <root@MSI>
2023-05-16 19:58:43 -0500

    afsdfdsg

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def delete_at(my_list=None, idx=0):
+    if my_list is None:
+        my_list = []
+    
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    
+    new_list = []
+    for i in range(len(my_list)):
+        if i != idx:
+            new_list.append(my_list[i])
+    
+    return new_list
