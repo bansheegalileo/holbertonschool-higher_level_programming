6479c1808d3bb4315afc36c8d1b99e5b89a8de7b
root <root@MSI>
2023-06-06 18:53:30 -0500

    eep

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,61 +8,110 @@ from models.base import Base
 
 class Rectangle(Base):
     """
-    as above
+    Represents a rectangle.
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initializes a Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int, optional): The x-coordinate of the rectangle's position. Defaults to 0.
+            y (int, optional): The y-coordinate of the rectangle's position. Defaults to 0.
+            id (int, optional): The ID of the rectangle. Defaults to None.
+        """
         super().__init__(id)
 
-        self.check_integer_parameter(height, 'height')
-        self.check_integer_parameter(width, 'width')
-        self.check_integer_parameter(x, 'x')
-        self.check_integer_parameter(y, 'y')
-
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
+        """
+        Getter for the width attribute.
+
+        Returns:
+            int: The width of the rectangle.
+        """
         return self.__width
 
     @width.setter
-    def width(self, param):
-        self.check_integer_parameter(param, 'width')
+    def width(self, value):
+        """
+        Setter for the width attribute.
 
-        self.__width = param
+        Args:
+            value (int): The new value for the width of the rectangle.
+        """
+        self.check_integer_parameter(value, "width")
+        self.__width = value
 
     @property
     def height(self):
+        """
+        Getter for the height attribute.
+
+        Returns:
+            int: The height of the rectangle.
+        """
         return self.__height
 
     @height.setter
-    def height(self, param):
-        self.check_integer_parameter(param, 'height')
+    def height(self, value):
+        """
+        Setter for the height attribute.
 
-        self.__height = param
+        Args:
+            value (int): The new value for the height of the rectangle.
+        """
+        self.check_integer_parameter(value, "height")
+        self.__height = value
 
     @property
     def x(self):
+        """
+        Getter for the x-coordinate attribute.
+
+        Returns:
+            int: The x-coordinate of the rectangle's position.
+        """
         return self.__x
 
     @x.setter
-    def x(self, param):
-        self.check_integer_parameter(param, 'x')
+    def x(self, value):
+        """
+        Setter for the x-coordinate attribute.
 
-        self.__x = param
+        Args:
+            value (int): The new value for the x-coordinate of the rectangle's position.
+        """
+        self.check_integer_parameter(value, "x")
+        self.__x = value
 
     @property
     def y(self):
+        """
+        Getter for the y-coordinate attribute.
+
+        Returns:
+            int: The y-coordinate of the rectangle's position.
+        """
         return self.__y
 
     @y.setter
-    def y(self, param):
-        self.check_integer_parameter(param, 'y')
+    def y(self, value):
+        """
+        Setter for the y-coordinate attribute.
 
-        self.__y = param
+        Args:
+            value (int): The new value for the y-coordinate of the rectangle's position.
+        """
+        self.check_integer_parameter(value, "y")
+        self.__y = value
 
     def check_integer_parameter(self, value, param):
         """
@@ -76,54 +125,11 @@ class Rectangle(Base):
             TypeError: If the value is not an integer.
             ValueError: If the value doesn't meet the specified constraints.
         """
-        if type(value) is not int:
-            raise TypeError(param + ' must be an integer')
+        if not isinstance(value, int):
+            raise TypeError(f"{param} must be an integer")
 
-        if value <= 0 and param in ('width', 'height'):
-            raise ValueError(param + ' must be > 0')
+        if value <= 0 and param in ("width", "height"):
+            raise ValueError(f"{param} must be > 0")
 
-        if value < 0 and param in ('x', 'y'):
-            raise ValueError(param + ' must be >= 0')
-
-    def area(self):
-        return self.__width * self.__height
-
-    def display(self):
-        if self.__y > 0:
-            print('\n' * self.__y, end='')
-
-        for i in range(self.height):
-            if self.__x > 0:
-                print(' ' * self.__x, end='')
-
-            print('#' * self.__width)
-
-    def __str__(self):
-        return '[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}'.format(
-            self.id, self.x, self.y, self.width, self.height
-        )
-
-    def update(self, *args, **kwargs):
-        argc = len(args)
-        kwargc = len(kwargs)
-        modif_attrs = ['id', 'width', 'height', 'x', 'y']
-
-        if argc > 5:
-            argc = 5
-
-        if argc > 0:
-            for i in range(argc):
-                setattr(self, modif_attrs[i], args[i])
-        elif kwargc > 0:
-            for k, v in kwargs.items():
-                if k in modif_attrs:
-                    setattr(self, k, v)
-
-    def to_dictionary(self):
-        return {
-            'id': self.id,
-            'width': self.width,
-            'height': self.height,
-            'x': self.x,
-            'y': self.y
-        }
+        if value < 0 and param in ("x", "y"):
+            raise ValueError(f"{param} must be >= 0")
