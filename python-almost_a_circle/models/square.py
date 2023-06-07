38fa962fbad4c12afb6b3bb9bc02788aab1e3d1a
root <root@MSI>
2023-06-06 20:03:38 -0500

    docum

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -3,29 +3,66 @@
 square model
 """
 
-from models.base import Base
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """
+    Represents a Square, a special type of Rectangle.
+    """
+
     def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializes a Square instance.
+
+        Args:
+            size (int): The size of the square.
+            x (int): The x-coordinate of the square's position.
+            y (int): The y-coordinate of the square's position.
+            id (int): The unique identifier of the square.
+        """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return '[Square] ({:d}) {:d}/{:d} - {:d}'.format(
+        """
+        Returns a string representation of the square.
+
+        Returns:
+            str: The string representation of the square.
+        """
+        return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width
         )
 
     @property
     def size(self):
+        """
+        Gets the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
         return self.width
 
     @size.setter
     def size(self, value):
+        """
+        Sets the size of the square.
+
+        Args:
+            value (int): The size to set for the square.
+        """
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
+        """
+        Updates the attributes of the square.
+
+        Args:
+            *args: Variable length arguments for attribute values.
+            **kwargs: Keyword arguments for attribute values.
+        """
         argc = len(args)
         kwargc = len(kwargs)
         modif_attrs = ['id', 'size', 'x', 'y']
@@ -42,6 +79,12 @@ class Square(Rectangle):
                     setattr(self, k, v)
 
     def to_dictionary(self):
+        """
+        Returns the dictionary representation of the square.
+
+        Returns:
+            dict: The dictionary representation of the square.
+        """
         return {
             'id': self.id,
             'size': self.size,
