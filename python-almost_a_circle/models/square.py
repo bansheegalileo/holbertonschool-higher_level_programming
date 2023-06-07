c266f62e835e14ec4508c7e9d1a2b375d01dd911
root <root@MSI>
2023-06-06 20:02:02 -0500

    swuarre

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,39 +1,50 @@
 #!/usr/bin/python3
 """
-the square class model
+square model
 """
 
+from models.base import Base
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
-    Represents a Square, a special type of Rectangle.
-    """
-
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        Initializes a Square instance.
-
-        Args:
-            size (int): The size of the square.
-            x (int): The x-coordinate of the square's position.
-            y (int): The y-coordinate of the square's position.
-            id (int): The unique identifier of the square.
-
-        Raises:
-            TypeError: If the size, x, or y parameters are not integers.
-            ValueError: If the size, x, or y parameters are invalid.
-        """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """
-        Returns a string representation of the square.
-
-        Returns:
-            str: The string representation of the square.
-        """
-        return "[Square] ({}) {}/{} - {}".format(
+        return '[Square] ({:d}) {:d}/{:d} - {:d}'.format(
             self.id, self.x, self.y, self.width
         )
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        argc = len(args)
+        kwargc = len(kwargs)
+        modif_attrs = ['id', 'size', 'x', 'y']
+
+        if argc > 4:
+            argc = 4
+
+        if argc > 0:
+            for i in range(argc):
+                setattr(self, modif_attrs[i], args[i])
+        elif kwargc > 0:
+            for k, v in kwargs.items():
+                if k in modif_attrs:
+                    setattr(self, k, v)
+
+    def to_dictionary(self):
+        return {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+        }
