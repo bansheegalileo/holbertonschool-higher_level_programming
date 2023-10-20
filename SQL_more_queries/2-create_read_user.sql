e42d6a1704f8f85622e0de7b47eb401878018526
root <root@MSI>
2023-10-19 22:31:07 -0500

    Fixing 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,4 @@
 -- Creates d8ab8se and user with select priv.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost; 
- 
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
